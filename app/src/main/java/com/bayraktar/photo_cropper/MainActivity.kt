3806a3b04a219c761c8e71f449b9f44be659ed23
emre.bayraktar <emre.bayraktar@ideateknoloji.com.tr>
2021-05-17 09:03:21 +0300

    base crop operations

--- app/src/main/java/com/bayraktar/photo_cropper/MainActivity.kt
+++ app/src/main/java/com/bayraktar/photo_cropper/MainActivity.kt
@@ -7,31 +7,46 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
 import android.view.ViewTreeObserver
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListener {
 
     private lateinit var rlImage: RelativeLayout
+    private lateinit var viewContent: View
     private lateinit var ivFileUpload: ImageView
     private lateinit var imageView: ImageView
+
+    private lateinit var cl3x3: ConstraintLayout
+    private lateinit var cl3x2: ConstraintLayout
+
+    private lateinit var tv3x2: TextView
+    private lateinit var tv3x3: TextView
+
     private lateinit var btnCrop: Button
 
     private var maxWidth: Int = 0
     private var maxHeight: Int = 0
 
-    private var widthRatio: Float = 0f
-    private var heightRatio: Float = 0f
+    private var is3x3: Boolean = false
+
+    private var dataUri: Uri? = null
+
+    private val ratio: Float
+        get() {
+            return if (is3x3) 1f else .666f
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -39,6 +54,13 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListene
         setContentView(R.layout.activity_main)
 
         rlImage = findViewById(R.id.rlImage)
+        viewContent = findViewById(R.id.viewContent)
+        cl3x3 = findViewById(R.id.cl3x3)
+        cl3x2 = findViewById(R.id.cl3x2)
+
+        tv3x2 = findViewById(R.id.tv3x2)
+        tv3x3 = findViewById(R.id.tv3x3)
+
         ivFileUpload = findViewById(R.id.ivFileUpload)
         imageView = findViewById(R.id.imageView)
         btnCrop = findViewById(R.id.btnCrop)
@@ -47,16 +69,18 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListene
             chooseImage()
         }
 
+        tv3x2.setOnClickListener {
+            addGrid(false)
+        }
+        tv3x3.setOnClickListener {
+            addGrid(true)
+        }
+
         findViewById<Button>(R.id.btnCrop).setOnClickListener {
             crop()
         }
-        init()
-    }
-
-    private fun init() {
-//        if (imageView.width != 0 && imageView.height != 0)
-
-        rlImage.viewTreeObserver.addOnGlobalLayoutListener(this)
+        initLayout()
+        addGrid(is3x3)
     }
 
     override fun onStart() {
@@ -85,13 +109,42 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListene
         }
     }
 
+    private fun addGrid(is3x3: Boolean) {
+        if (this.is3x3 == is3x3) return
+
+        this.is3x3 = is3x3
+        if (is3x3) {
+            cl3x3.visibility = View.VISIBLE
+            cl3x2.visibility = View.GONE
+            tv3x3.background =
+                ContextCompat.getDrawable(this, R.drawable.bakcground_button_selected)
+            tv3x2.background = ContextCompat.getDrawable(this, R.drawable.background_button_default)
+        } else {
+            cl3x3.visibility = View.GONE
+            cl3x2.visibility = View.VISIBLE
+            tv3x3.background = ContextCompat.getDrawable(this, R.drawable.background_button_default)
+            tv3x2.background =
+                ContextCompat.getDrawable(this, R.drawable.bakcground_button_selected)
+        }
+        initLayout()
+    }
+
+    private fun initLayout() {
+        rlImage.viewTreeObserver.addOnGlobalLayoutListener(this)
+    }
+
     private fun chooseImage() {
         val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         resultLauncher.launch(pickPhoto)
     }
 
     private fun crop() {
-        val bitmapList = cropImageByGrid((imageView.drawable as BitmapDrawable).bitmap)
+        if (imageView.drawable !is BitmapDrawable) {
+            chooseImage()
+            return
+        }
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val bitmapList = cropImageByGrid(bitmap)
 
         val imageRepository = ImageRepository(this)
 
@@ -106,6 +159,80 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListene
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 
     }
+
+
+    private fun cropImageByGrid(bitmap: Bitmap): List<Bitmap> {
+        val width = bitmap.width
+        val height = bitmap.height
+        val bitmapList: ArrayList<Bitmap> = ArrayList()
+        val horizontal = 3
+        val vertical = if (is3x3) 3 else 2
+        val freqWidth = width / horizontal
+        val freqHeight = height / vertical
+        for (i in 0 until horizontal) {
+            for (j in 0 until vertical) {
+                val startX = freqWidth * i
+                val startY = freqHeight * j
+                val tempBitmap = Bitmap.createBitmap(bitmap, startX, startY, freqWidth, freqHeight)
+                bitmapList.add(tempBitmap)
+            }
+        }
+
+        return bitmapList
+    }
+
+    private fun getResultBitmap(bm: Bitmap): Bitmap {
+        val backgroundBitmap = getBackground()
+        val resultBitmap = Bitmap.createBitmap(
+            backgroundBitmap.width,
+            backgroundBitmap.height,
+            backgroundBitmap.config
+        )
+        val canvas = Canvas(resultBitmap)
+        canvas.drawColor(Color.WHITE)
+        canvas.drawBitmap(backgroundBitmap, Matrix(), null)
+        canvas.drawBitmap(
+            bm,
+            (backgroundBitmap.width - bm.width).toFloat() / 2,
+            (backgroundBitmap.height - bm.height).toFloat() / 2,
+            Paint()
+        )
+        return resultBitmap
+    }
+
+    private fun getBackground(): Bitmap {
+        return Bitmap.createBitmap(
+            maxWidth,
+            maxHeight,
+            Bitmap.Config.ARGB_8888
+        )
+    }
+
+    private fun scaleBitmap(bm: Bitmap): Bitmap {
+        var width = bm.width
+        var height = bm.height
+        when {
+            width > height -> {
+                // landscape
+                val ratio = width.toFloat() / maxWidth
+                width = maxWidth
+                height = (height / ratio).toInt()
+            }
+            height > width -> {
+                // portrait
+                val ratio = height.toFloat() / maxHeight
+                height = maxHeight
+                width = (width / ratio).toInt()
+            }
+            else -> {
+                // square
+                height = maxHeight
+                width = maxWidth
+            }
+        }
+        return Bitmap.createScaledBitmap(bm, width, height, true)
+    }
+
 
     private fun showInContextUI() {
         AlertDialog.Builder(this)
@@ -143,91 +270,24 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListene
             if (result.resultCode == Activity.RESULT_OK) {
                 // There are no request codes
                 val data: Intent? = result.data
-                imageView.setImageURI(data!!.data)
-                imageView.setImageBitmap(getResultBitmap((imageView.drawable as BitmapDrawable).bitmap))
+                dataUri = data!!.data
+                imageView.setImageURI(dataUri)
+                imageView.setImageBitmap(getResultBitmap(scaleBitmap((imageView.drawable as BitmapDrawable).bitmap)))
             }
         }
-
-    private fun cropImageByGrid(bitmap: Bitmap): List<Bitmap> {
-        val width = bitmap.width
-        val height = bitmap.height
-        val bitmapList: ArrayList<Bitmap> = ArrayList()
-        val freqWidth = width / 3
-        val freqHeight = height / 2
-        for (i in 0..2) {
-            for (j in 0..1) {
-                val startX = freqWidth * i
-                val startY = freqHeight * j
-                val tempBitmap = Bitmap.createBitmap(bitmap, startX, startY, freqWidth, freqHeight)
-                bitmapList.add(tempBitmap)
-            }
-        }
-
-        return bitmapList
-    }
-
-    private fun getResultBitmap(bm: Bitmap): Bitmap {
-        val backgroundBitmap = Bitmap.createBitmap(
-            maxWidth,
-            maxHeight,
-            Bitmap.Config.ARGB_8888
-        )
-        val bitmapToDrawInTheCenter = scaleBitmap(bm)
-        val resultBitmap = Bitmap.createBitmap(
-            backgroundBitmap.width,
-            backgroundBitmap.height,
-            backgroundBitmap.config
-        )
-        val canvas = Canvas(resultBitmap)
-        canvas.drawColor(Color.WHITE)
-        canvas.drawBitmap(backgroundBitmap, Matrix(), null)
-        canvas.drawBitmap(
-            bitmapToDrawInTheCenter,
-            (backgroundBitmap.width - bitmapToDrawInTheCenter.width).toFloat() / 2,
-            (backgroundBitmap.height - bitmapToDrawInTheCenter.height).toFloat() / 2,
-            Paint()
-        )
-        return resultBitmap
-    }
-
-    private fun scaleBitmap(bm: Bitmap): Bitmap {
-        var width = bm.width
-        var height = bm.height
-        when {
-            width > height -> {
-                // landscape
-                val ratio = width.toFloat() / maxWidth
-                width = maxWidth
-                height = (height / ratio).toInt()
-            }
-            height > width -> {
-                // portrait
-                val ratio = height.toFloat() / maxHeight
-                height = maxHeight
-                width = (width / ratio).toInt()
-            }
-            else -> {
-                // square
-                height = maxHeight
-                width = maxWidth
-            }
-        }
-        return Bitmap.createScaledBitmap(bm, width, height, true)
-    }
-
 
     override fun onGlobalLayout() {
         rlImage.viewTreeObserver.removeOnGlobalLayoutListener(this)
-        val width: Int = rlImage.measuredWidth
-        val height: Int = rlImage.measuredHeight
+        val width: Int = viewContent.measuredWidth
+        val height: Int = viewContent.measuredHeight
         if (width != 0 && height != 0) {
             val params = if (width > height) {
-                val newHeight = height * .666
+                val newHeight = height * ratio
                 maxWidth = height
                 maxHeight = newHeight.toInt()
                 RelativeLayout.LayoutParams(height, newHeight.toInt())
             } else {
-                val newHeight = width * .666
+                val newHeight = width * ratio
                 maxWidth = width
                 maxHeight = newHeight.toInt()
                 RelativeLayout.LayoutParams(width, newHeight.toInt())
@@ -235,6 +295,10 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnGlobalLayoutListene
             params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE)
             rlImage.layoutParams = params
         }
+
+        if (dataUri == null) return
+        imageView.setImageURI(dataUri)
+        imageView.setImageBitmap(getResultBitmap(scaleBitmap((imageView.drawable as BitmapDrawable).bitmap)))
     }
 
 }
